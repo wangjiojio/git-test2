4539fef02fd582e1a48c37d5485f46877f7df037
wangjiojio <68528392+wangjiojio@users.noreply.github.com>
2022-06-09 22:12:29 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("test master");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
 
 
     }
